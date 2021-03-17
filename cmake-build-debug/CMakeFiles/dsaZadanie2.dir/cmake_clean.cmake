42971bbfd36e99f461959074c2ccfc3278e24bdb
Pavol Hradsky <hradskypalo@gmail.com>
2021-03-17 10:43:50 +0100

    add idea

---
+++ cmake-build-debug/CMakeFiles/dsaZadanie2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsaZadanie2.dir/main.c.obj"
+  "dsaZadanie2.exe"
+  "dsaZadanie2.exe.manifest"
+  "dsaZadanie2.pdb"
+  "libdsaZadanie2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dsaZadanie2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
